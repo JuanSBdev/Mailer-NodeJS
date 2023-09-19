ccf398106d8123634bcbe0b9cd468c46ea4a902a
Juan SB <juansb.dev@gmail.com>
2023-09-19 17:04:41 -0300

    FIX - PORT & CORSHEADER

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require ('express')
 const router = require ('./routes/index')
 
 const app = express()
-const PORT = 3001
+const PORT = 8080
 
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173')
+    res.header('Access-Control-Allow-Origin', 'https://portajlsb.netlify.app')
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header(
       'Access-Control-Allow-Headers',
